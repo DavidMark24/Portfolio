fa20baabaf8bbacfca1f84c9a5c25bf438773232
David Johnson <david.m.johnson32@gmail.com>
2021-04-04 22:58:58 -0700

    Updated portfolio componet

--- src/componets/Portfolio/Portfolio.js
+++ src/componets/Portfolio/Portfolio.js
@@ -12,7 +12,7 @@ const Portfolio = () => {
                   alt="city website" srcset=""/> </a>
                </div>
                <div class="col col-12 col-lg-9 col-xl-9 col-xxl-9">
-                <p class="pro">CtyFaktz! City search made fun! <br/>
+                <p class="pro">CtyFaktz! City search made fun. <br/>
                   A website that has useful and fun facts about your favorite cities. <br/>
                   It's a new way of looking at your favorite towns!</p>
                </div>
@@ -34,11 +34,36 @@ const Portfolio = () => {
             <div class="col col-12 col-lg-3 col-xl-3 col-xxl-3">
               <a href="https://pacific-lowlands-24304.herokuapp.com/index"> <img class="city" src="pictures/burger.png"
                   alt="burger"/> </a>
-               </div>  <div class="col col-12 col-lg-9 col-xl-9 col-xxl-9">
-                <p class="pro"> Eat Da Burger! <br/>
+               </div>  
+               <div class="col col-12 col-lg-9 col-xl-9 col-xxl-9">
+                <p class="pro"> Eat Da Burger <br/>
                   A Node.js , Express and Handlebars application that allows you to input your favorite burgers and devour them! <br/>
                    I used MySQL to create the database that holds all the information. <br/>
                     Also deployed the app using Heroku you can see below.
+                  </p>
+               </div>
+          </div>
+          <div class="row">
+            <div class="col col-12 col-lg-3 col-xl-3 col-xxl-3">
+              <a href="https://intense-castle-01702.herokuapp.com/stats"> <img class="city" src="pictures/fitness.png"
+                  alt="burger"/> </a>
+               </div>  
+               <div class="col col-12 col-lg-9 col-xl-9 col-xxl-9">
+                <p class="pro"> Fitness Tracker <br/>
+                 A fitness app that allows to track all of your workouts <br/>
+                  By storing all the data with MongoDB we can keep a record of all you completed and occuring workouts. <br/>
+                  </p>
+               </div>
+          </div>
+          <div class="row">
+            <div class="col col-12 col-lg-3 col-xl-3 col-xxl-3">
+              <a href="https://radiant-chamber-40061.herokuapp.com/"> <img class="city" src="pictures/employeeD.png"
+                  alt="burger"/> </a>
+               </div>  
+               <div class="col col-12 col-lg-9 col-xl-9 col-xxl-9">
+                <p class="pro"> Employee Directory <br/>
+                 A React application that stores employee information<br/>
+                  Using React and Axios you can search for your employees by name, birthday or email.<br/>
                   </p>
                </div>
           </div>
